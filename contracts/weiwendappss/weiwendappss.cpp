d5b39e122290c64a3c3c82a4897d7bed4aeda473
songguo6 <zhanglisong25@163.com>
2019-05-11 23:57:43 +0800

    contracts

--- contracts/weiwendappss/weiwendappss.cpp
+++ contracts/weiwendappss/weiwendappss.cpp
@@ -1,7 +1,46 @@
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
+#include <eosio/system.hpp>
+#include <eosio/crypto.hpp>
+
+#define TOKEN_SYMBOL "WEI"
 
 using namespace eosio;
+
+class random_gen {
+private:
+  static random_gen instance;
+
+  const uint32_t a = 1103515245;
+  const uint32_t c = 12345;
+  uint64_t seed = 0;
+
+public:
+  static random_gen& get_instance(name account) {
+    if (instance.seed == 0) {
+      instance.seed = current_time_point().time_since_epoch().count() + account.value;
+    }
+    return instance;
+  }
+
+  uint32_t range(uint32_t to) {
+    checksum256 result = sha256((char *)&seed, sizeof(seed));
+    auto arr = result.extract_as_byte_array();
+    seed = arr[1];
+    seed <<= 32;
+    seed |= arr[0];
+    return (uint32_t)(seed % to);
+  }
+
+  uint32_t range(uint32_t from, uint32_t to) {
+    if(from == to){
+      return from;
+    }else if(from < to){
+      return range(to - from + 1) + from;
+    }
+    return range(to);
+  }
+};
 
 CONTRACT weiwendappss : public eosio::contract {
 
@@ -10,7 +49,24 @@ public:
 
 
   ACTION reward(name account){
+    require_auth(account);
 
+    user_t users(_self, _self.value);    
+    auto itr = users.find(account.value);
+    
+    if(itr == users.end()){
+      itr = users.emplace(account, [&](auto& user){
+        user.account = account;
+        user.balance = asset(0, symbol(TOKEN_SYMBOL, 4));
+        user.follow_num = 0;   
+        user.fans_num = 0;             
+        user.post_num = 0;              
+        user.like_num = 0;              
+        user.last_reward_time = time_point_sec();
+        user.last_like_time = time_point_sec();  
+        user.like_times = 0;             
+      });
+    }
   }
 
   ACTION deposit(name account, asset quantity){
@@ -21,7 +77,7 @@ public:
 
   }
 
-  ACTION post(name author, const std::string& content, uint8_t attachtype, const std::string& attachment){
+  ACTION post(name author, const std::string& content, uint32_t attachtype, const std::string& attachment){
     
   }
 
@@ -29,7 +85,7 @@ public:
 
   }
 
-  ACTION like(name author, uint8_t type, uint64_t type_id){
+  ACTION like(name author, uint32_t type, uint64_t type_id){
 
   }
 
@@ -70,7 +126,7 @@ private:
     uint32_t like_num;              //获赞数
     time_point_sec last_reward_time;//上次领币时间
     time_point_sec last_like_time;  //上次点赞时间
-    uint8_t like_times;             //当天已点赞次数
+    uint32_t like_times;            //当天已点赞次数
 
     uint64_t primary_key() const { return account.value; }
   };
@@ -82,7 +138,7 @@ private:
     uint64_t id;              //自增id
     name author;              //作者
     std::string content;      //内容
-    uint8_t attachtype;       //附件类型 0=无 1=url 2=ipfs hash 3=file
+    uint32_t attachtype;      //附件类型 0=无 1=url 2=ipfs hash 3=file
     std::string attachment;   //附件
     time_point_sec time;      //创建时间
     asset balance;            //获得代币数
@@ -121,7 +177,7 @@ private:
   //点赞表
   TABLE liketable {
     uint64_t id;              //自增id
-    uint8_t type;             //点赞类型 1=微文点赞 2=评论点赞    
+    uint32_t type;            //点赞类型 1=微文点赞 2=评论点赞    
     uint64_t type_id;         //微文或评论的id
     name author;              //点赞者
 
