9f455939974f755cfa8b37337a638f179fc2311d
songguo6 <zhanglisong25@163.com>
2019-05-17 23:50:16 +0800

    redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import store from './store';
+
 function App() {
-  return (<div>Hello EOS</div>);
+  return (
+    <div>Hello EOS：{store.getState().account}</div>
+  );
 }
 
 export default App;
---
+++ frontend/src/store/reducer.js
@@ -0,0 +1,7 @@
+const defaultState = {
+  account: 'songguo55555'
+}
+
+export default (state = defaultState, action) => {
+  return state;
+}
