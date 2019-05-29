b734ac999455dd4053b10356798d1c865de8fbd6
songguo6 <zhanglisong25@163.com>
2019-05-29 23:18:38 +0800

    config

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -9,9 +9,9 @@ const contract = 'weiwendappss';
 // jungle testnet
 const network = {
   blockchain: 'eos',
-  protocol: 'http',
+  protocol: 'https',
   host: 'jungle2.cryptolions.io',
-  port: 80,
+  port: 443,
   chainId: 'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473',
 };
 
