f3b034eb9cc4a0bdb74dd8024fe066d36bbe94cf
songguo6 <zhanglisong25@163.com>
2019-05-29 08:46:43 +0800

    user

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
-import { Row, Col, Card, Divider, Avatar } from 'antd';
+import { Row, Col, Card, Divider, Avatar, Popover, Modal, Button } from 'antd';
 import { Link } from 'react-router-dom';
 
 class User extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleDeposit = this.handleDeposit.bind(this);
+    this.handleWithdraw = this.handleWithdraw.bind(this);
+    this.handleVisibleChange = this.handleVisibleChange.bind(this);
+  }
+
+  state = {
+    popoverVisible: false,
+  };
 
   renderCol(label, value){
     return (
@@ -14,6 +25,18 @@ class User extends Component {
       </Col>
     );
   }
+
+  handleDeposit(){
+    this.setState({popoverVisible: false});
+  };
+
+  handleWithdraw(){
+    this.setState({popoverVisible: false});
+  }
+
+  handleVisibleChange(visible){
+    this.setState({ popoverVisible:visible });
+  };
 
   render(){
     const { logged, user } = this.props;
@@ -34,14 +57,27 @@ class User extends Component {
           <Divider style={{ marginTop: 20 }} dashed />
           <Row gutter={0} className='user'>
             <Col className='item'>
-              <Link to='/'>
-                <span>获得收益 </span>{user.account ? user.balance : ''} 
-                <Avatar 
-                  size="small" 
-                  style={{marginLeft: 5 }}
-                  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYWKv4dCST42_uNTYKoTiuPZ11GP5JXbSjOHQ7MgNNZ9zbqqGa'
-                />
-              </Link>
+              <Popover
+                content={
+                  <div>
+                    <Button style={{margin: '0 10px'}} onClick={this.handleDeposit}>充值</Button>
+                    <Button style={{margin: '0 10px'}} onClick={this.handleWithdraw}>提现</Button>    
+                  </div>
+                }
+                title={user.account ? user.balance : 'WEI'} 
+                trigger='click'
+                visible={this.state.popoverVisible}
+                onVisibleChange={this.handleVisibleChange}
+              >
+                <Link to='/'>
+                  <span>获得收益 </span>{user.account ? user.balance : ''} 
+                  <Avatar 
+                    size="small" 
+                    style={{marginLeft: 5 }}
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYWKv4dCST42_uNTYKoTiuPZ11GP5JXbSjOHQ7MgNNZ9zbqqGa'
+                  />
+                </Link>
+              </Popover>
             </Col>
           </Row>
         </div>
